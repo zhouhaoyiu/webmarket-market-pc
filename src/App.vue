7b44272ee5bb22b5464d6234188acf0a1e7e8485
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2022-02-23 18:38:04 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <TopBar></TopBar>
     <router-view />
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import TopBar from "./components/topBar.vue";
+@Component({
+  components: { TopBar },
+})
+export default class App extends Vue {}
+</script>
+
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/components/topBar.vue
+++ src/components/topBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home"></div>
+  <div>网上商城</div>
 </template>
 
 <script lang="ts">
@@ -8,5 +8,5 @@ import { Component, Vue } from "vue-property-decorator";
 @Component({
   components: {},
 })
-export default class Home extends Vue {}
+export default class TopBar extends Vue {}
 </script>
