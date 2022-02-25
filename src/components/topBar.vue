92181907a8615d343ac3776eca1f8f2e50166bed
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2022-02-25 21:24:39 +0800

    init

--- src/components/topBar.vue
+++ src/components/topBar.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>网上商城</div>
+  <div class="top-bar">
+    <div class="marketName" v-if="marketInfo">
+      欢迎来到{{ marketInfo.marketName }}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,5 +12,29 @@ import { Component, Vue } from "vue-property-decorator";
 @Component({
   components: {},
 })
-export default class TopBar extends Vue {}
+export default class TopBar extends Vue {
+  private marketInfo = null;
+  async mounted(): Promise<void> {
+    // do something
+    const res = await this.axios.get("/marketInfo/getMarketInfo");
+    this.marketInfo = this._.cloneDeep(res.data.data[0]);
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.top-bar {
+  height: 50px;
+  line-height: 50px;
+  font-weight: bold;
+  width: 100%;
+  background: gray;
+  display: flex;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
+  .marketName {
+    // color: white;
+    margin-left: 40px;
+    font-size: 20px;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import Vue from "vue";
+import Vuex from "vuex";
 import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import router from "./router";
 import store from "./store";
+import ElementUI from "element-ui";
+import "./assets/reset.scss";
+import "element-ui/lib/theme-chalk/index.css";
+import _ from "lodash";
 
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
+Vue.prototype.$axios.defaults.baseURL = "http://localhost:8090/";
+Vue.use(ElementUI);
+Vue.use(VueAxios, axios);
+Vue.use(Vuex);
+Vue.prototype._ = _;
 
 new Vue({
   router,
