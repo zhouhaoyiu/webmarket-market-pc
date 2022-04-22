7a8894feaf658125e3cee7e33947dc282fb8d353
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2022-04-22 20:48:54 +0800

    Update recommendGoods.vue

--- src/components/recommendGoods.vue
+++ src/components/recommendGoods.vue
@@ -1,20 +1,99 @@
 <template>
   <div class="recommend">
-    <div class="recommend-title">推荐商品</div>
-    <div>
-
+    <div class="recommend-title">推荐商品 {{ recommendName }}</div>
+    <div style="display: flex; justify-content: space-between">
+      <div
+        @mousedown="preventDefault()"
+        @click="goGoodDetail(good.goodId)"
+        style="cursor: pointer; user-select: none"
+        v-for="good in recommendGoods.slice(0, 8)"
+        :key="good.goodId"
+      >
+        <el-tooltip
+          class="item"
+          effect="dark"
+          :content="good.goodName"
+          placement="top"
+        >
+          <img
+            style="width: 100px; height: 100px"
+            :src="`http://localhost:8090/images/${
+              good.goodImages.split(',')[0]
+            }`"
+            alt=""
+          />
+        </el-tooltip>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+enum goodRecommend {
+  sellVolume = "销量",
+  visitVolume = "访问量",
+  shelfTime = "上架时间(新->旧)",
+}
 import Vue from "vue";
 import Component from "vue-class-component";
 @Component({
   components: {},
 })
 export default class RecommendGoods extends Vue {
-  mounted() {
+  public get recommendName(): string {
+    switch (this.recommend) {
+      case "sellVolume":
+        return "热销";
+      case "visitVolume":
+        return "热访";
+      case "shelfTime":
+        return "新品";
+      default:
+        return "";
+    }
+  }
+
+  public get recommend(): string {
+    return this.$store.state.marketInfo.marketRecommend;
+  }
+
+  public get recommendGoods(): any[] {
+    switch (this.recommend) {
+      case "sellVolume":
+        return this.$store.state.goodsList.sort(
+          (a: { goodSellCount: number }, b: { goodSellCount: number }) =>
+            b.goodSellCount - a.goodSellCount
+        );
+      case "visitVolume":
+        return this.$store.state.goodsList.sort(
+          (a: { goodVisitCount: number }, b: { goodVisitCount: number }) =>
+            b.goodVisitCount - a.goodVisitCount
+        );
+      case "shelfTime":
+        return this.$store.state.goodsList.sort(
+          (a: { goodId: number }, b: { goodId: number }) => b.goodId - a.goodId
+        );
+      default:
+        return this.$store.state.goodsList;
+    }
+  }
+
+  public preventDefault(): void {
+    event!.preventDefault();
+  }
+
+  public goGoodDetail(id: number): void {
+    this.$nextTick(() => {
+      this.$router.push({
+        path: "/home/detail",
+        query: {
+          goodId: "" + id,
+        },
+      });
+    });
+  }
+
+  public mounted() {
     // console.log("RecommendGoods mounted");
   }
 }
