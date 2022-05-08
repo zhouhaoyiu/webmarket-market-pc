8bbb23b914918c02729eea550016414fb7174dad
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2022-05-08 14:37:30 +0800

    add
    Signed-off-by: zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>

--- commit.sh
+++ commit.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+sign=true
+message="some changes"
+for i in $*
+do
+  index=$[index+1]
+  if [ $i = "-ns" ]
+  then
+    sign=false
+  fi
+  if [ $i = "-m" ]
+  then
+    _index=`expr $index + 1`
+    message=$(echo $* | cut -d " " -f $_index)
+  fi
+done
+
 git add .
-git commit -s -m "add"
+if [ $sign = true ]
+then
+  git commit -s -m "$message"
+else
+  git commit -m "$message"
+fi
 git push
